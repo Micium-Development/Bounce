98fcf3d65de8076afd10e609b253fbcf0997411b
growtoups <67888122+growtoups@users.noreply.github.com>
2022-05-15 08:25:49 -0400

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,5 +1,5 @@
 module.exports = async (client) => {
 client.manager.init(client.user.id);
-console.log(`Bot online now`);
+console.log(`Thanks for using Bounce from Micium-Development https://github.com/Micium-Development/Bounce`);
 setInterval(() => {client.user.setActivity(`Made by https://micium-development.ovh/`, {type: 'LISTENING'})}, 60000);
 };
